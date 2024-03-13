2fbc9dc777380218cb1aa11160f9c657d9562af4
Ane <ane@Ane-MacBook-Pro.lan>
2024-03-13 21:56:22 +0100

    Add basic demographic parameters

---
+++ src/parameters/params.py
@@ -0,0 +1,9 @@
+# Initial populations
+initial_christian_population = 40_000
+initial_pagan_population = 60_000_000
+
+# Demographic parameters
+natural_birth_rate = 1 / (25 * 365)
+natural_death_rate = 1 / (25 * 365)
+
+# Infection parameters
