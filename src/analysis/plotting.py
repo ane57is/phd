647df57522fbce563753610310a7f530b154b075
Ane <ane@Ane-MacBook-Pro.lan>
2024-03-17 20:48:45 +0100

    refactor: update the 1.1 models' file and plotting functionality for reusability and add two proofs of concept to see that the whole thing behaves as expected (it appears to do so)

--- src/analysis/plotting.py
+++ src/analysis/plotting.py
@@ -1,41 +1,163 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.integrate import odeint
+from scipy.integrate import solve_ivp
 from src.models.type_11_models.seir import direct_transmission_over_two_connected_subpopulations_seird_model
+from src.models.type_11_models.seir import direct_transmission_over_one_population_as_in_plos_paper
 from src.parameters.params import default_seir_params
 from src.parameters.params import initial_christian_population
 from src.parameters.params import initial_pagan_population
 
 
-# Initial conditions
-y0 = [initial_christian_population - 1, 0, 1, 0, 0, 0, initial_pagan_population - 1, 0, 1, 0, 0, 0]
+def plot_seir_model(
+        solution,
+        t,
+        start_year,
+        end_year,
+        compartment_indices,
+        compartment_labels,
+        every_nth_year=5,  # TODO: rename to something more label-indicating
+        y_tick_interval=100_000,
+        display_y_label_every_n_ticks=1,
+        x_label='Time (years)',
+        y_label='Number of Individuals',
+        plot_title='SEIR Model Over Time'
+):
+    """
+    Plots the compartments of an SEIR model given the solution object, time array,
+    and the start and end year for x-axis labeling.
 
-# Time vector
-t = np.linspace(165, 189, (189 - 165 + 1) * 365)
+    Parameters:
+    - solution: The solution object returned by scipy.integrate.solve_ivp.
+    - t: The time points at which the solution was evaluated.
+    - start_year: The starting year of the simulation for labeling the x-axis.
+    - end_year: The ending year of the simulation for labeling the x-axis.
+    - compartment_indices: A list of indices for the compartments to be plotted.
+    - compartment_labels: A list of labels corresponding to the compartments.
+    - every_nth_year: Interval for labeling years on the x-axis, defaults to 5.
+    - y_tick_interval: Interval between y-axis ticks.
+    - display_y_label_every_n_ticks: Controls which y-axis labels are displayed.
+    - x_label: Label for the x-axis.
+    - y_label: Label for the y-axis.
+    - plot_title: Title of the plot.
+    """
+
+    fig, ax = plt.subplots(figsize=(10, 6))
+
+    # Plot the specified compartments
+    for index, label in zip(compartment_indices, compartment_labels):
+        ax.plot(t, solution.y[index, :], label=label)
+
+    # Set up the x-axis with year ticks and labels
+    total_days = (end_year - start_year + 1) * 365
+    year_tick_positions = np.arange(0, total_days, 365)
+    year_tick_labels = np.arange(start_year, end_year + 1)
+    ax.set_xticks(year_tick_positions)
+    ax.set_xticklabels(year_tick_labels, rotation=45)
+
+    # Optionally show every nth year label to avoid clutter
+    for index, label in enumerate(ax.xaxis.get_ticklabels()):
+        if index % every_nth_year != 0:
+            label.set_visible(False)
+
+    # Set up the y-axis with custom ticks based on the maximum value across specified compartments
+    y_max = np.max([np.max(solution.y[i, :]) for i in compartment_indices])
+    y_ticks = np.arange(0, y_max + y_tick_interval, y_tick_interval)
+    y_labels = [str(int(y)) if index % display_y_label_every_n_ticks == 0 else '' for index, y in enumerate(y_ticks)]
+    ax.set_yticks(y_ticks)
+    ax.set_yticklabels(y_labels)
+
+    # Set axis' labels and plot title
+    ax.set_xlabel(x_label)
+    ax.set_ylabel(y_label)
+    ax.set_title(plot_title)
+    ax.legend()
+
+    plt.tight_layout()
+    plt.show()
 
 
-# Solve the ODE
-solution = odeint(direct_transmission_over_two_connected_subpopulations_seird_model, y0, t, args=(default_seir_params,))
+def proof_of_concept_solve_and_plot_ap_as_smallpox_in_rome(start_year=165, end_year=189):
+    """
+    Solve and plot the case of my PLOS ONE article as a proof of concept (showcase this approach works).
+    """
+    # Initial conditions (just the population of the city of Rome and 1 infected - no other people,
+    # and not distinguishing Christians and Pagans.
+    y0 = [0, 0, 0, 0, 0, 0, 923405, 0, 1, 0, 0, 0]
 
-# Plotting
-fig, ax = plt.subplots(figsize=(10, 6))
+    # Timeframe of simulation
+    total_days = (end_year - start_year + 1) * 365
+    t = np.arange(0, total_days)
 
-ax.plot(t, solution[:, 0], label='Susceptible Christians')
-ax.plot(t, solution[:, 1], label='Exposed Christians')
-ax.plot(t, solution[:, 2], label='Infected Christians')
-ax.plot(t, solution[:, 3], label='Recovered Christians')
-ax.plot(t, solution[:, 4], label='Deceased Christians')
+    # Solve the ODE
+    def wrapper_for_solve_ivp(t, y):
+        return direct_transmission_over_one_population_as_in_plos_paper(y, t, default_seir_params)
+    solution = solve_ivp(wrapper_for_solve_ivp, [0, total_days], y0, method='BDF', t_eval=t)
 
-ax.plot(t, solution[:, 6], label='Susceptible Pagans')
-ax.plot(t, solution[:, 7], label='Exposed Pagans')
-ax.plot(t, solution[:, 8], label='Infected Pagans')
-ax.plot(t, solution[:, 9], label='Recovered Pagans')
-ax.plot(t, solution[:, 10], label='Deceased Pagans')
+    # Solution indices and labels relevant only to the Pagan compartments
+    # (except for the A compartment, dead due to age and other natural causes).
+    compartment_indices = [6, 7, 8, 9, 10]
+    compartment_labels = [
+        'Susceptible Pagans',
+        'Exposed Pagans',
+        'Infected Pagans',
+        'Recovered Pagans',
+        'Deceased Pagans'
+    ]
+    plot_seir_model(
+        solution,
+        t,
+        start_year=start_year,
+        end_year=end_year,
+        compartment_indices=compartment_indices,
+        compartment_labels=compartment_labels
+    )
 
-ax.set_xlabel('Time (years)')
-ax.set_ylabel('Number of Individuals')
-ax.set_title('SEIRD Model Over Time for Two Connected Subpopulations')
-ax.legend()
 
-plt.tight_layout()
-plt.show()
+def proof_of_concept_solve_and_plot_ap_as_smallpox_over_two_subpopulations_in_empire(
+        start_year=165,
+        end_year=189,
+        initial_christian_population=initial_christian_population,
+        initial_pagan_population=initial_pagan_population
+):
+    # Initial conditions (Christian and Pagan populations defined in src.parameters.params)
+    y0 = [initial_christian_population - 1, 0, 1, 0, 0, 0, initial_pagan_population - 1, 0, 1, 0, 0, 0]
+
+    # Timeframe of simulation
+    total_days = (end_year - start_year + 1) * 365
+    t = np.arange(0, total_days)
+
+    # Solve the ODE
+    def wrapper_for_solve_ivp(t, y):
+        return direct_transmission_over_two_connected_subpopulations_seird_model(y, t, default_seir_params)
+    solution = solve_ivp(wrapper_for_solve_ivp, [0, total_days], y0, method='BDF', t_eval=t)
+
+    # Solution indices and labels relevant to both Christian and Pagan compartments
+    # (except for the A compartment, dead due to age and other natural causes).
+    compartment_indices = [0, 1, 2, 3, 4, 6, 7, 8, 9, 10]
+    compartment_labels = [
+        'Susceptible Christians',
+        'Exposed Christians',
+        'Infected Christians',
+        'Recovered Christians',
+        'Deceased Christians',
+        'Susceptible Pagans',
+        'Exposed Pagans',
+        'Infected Pagans',
+        'Recovered Pagans',
+        'Deceased Pagans'
+    ]
+    plot_seir_model(
+        solution,
+        t,
+        start_year=start_year,
+        end_year=end_year,
+        compartment_indices=compartment_indices,
+        compartment_labels=compartment_labels,
+        every_nth_year=5,
+        y_tick_interval=1_000_000,
+        display_y_label_every_n_ticks=10,
+        plot_title='Antonine Plague as smallpox over two subpopulations in the whole Empire'
+    )
+
+
+proof_of_concept_solve_and_plot_ap_as_smallpox_over_two_subpopulations_in_empire()
--- src/models/type_11_models/seir.py
+++ src/models/type_11_models/seir.py
@@ -1,7 +1,3 @@
-import numpy as np
-from scipy.integrate import odeint
-
-
 def direct_transmission_over_two_connected_subpopulations_seird_model(y, t, parameters):
     s_c, e_c, i_c, r_c, d_c, a_c, s_p, e_p, i_p, r_p, d_p, a_p = y
     beta = parameters.beta
@@ -12,8 +8,8 @@ def direct_transmission_over_two_connected_subpopulations_seird_model(y, t, para
     d = parameters.natural_death_rate
 
     # Totals of subpopulations and whole population
-    n_c = s_c + e_c + i_c + r_c + d_c
-    n_p = s_p + e_p + i_p + r_p + d_p
+    n_c = s_c + e_c + i_c + r_c
+    n_p = s_p + e_p + i_p + r_p
     n = n_c + n_p
 
     # Christian compartments:
@@ -27,6 +23,38 @@ def direct_transmission_over_two_connected_subpopulations_seird_model(y, t, para
     # Pagan compartments:
     ds_p = b * n_p - beta * s_p * (i_c + i_p) / n - d * s_p
     de_p = beta * s_p * (i_c + i_p) / n - sigma * e_p - d * e_p
+    di_p = sigma * e_p - gamma * i_p - d * i_p
+    dr_p = (1 - fatality_rate) * gamma * i_p - d * r_p
+    dd_p = fatality_rate * gamma * i_p
+    da_p = d * n_p
+
+    # Return the derivatives in the same order
+    return [ds_c, de_c, di_c, dr_c, dd_c, da_c, ds_p, de_p, di_p, dr_p, dd_p, da_p]
+
+
+def direct_transmission_over_one_population_as_in_plos_paper(y, t, parameters):
+    s_c, e_c, i_c, r_c, d_c, a_c, s_p, e_p, i_p, r_p, d_p, a_p = y
+    beta = parameters.beta
+    sigma = parameters.sigma
+    gamma = parameters.gamma
+    fatality_rate = parameters.fatality_rate
+    b = parameters.natural_birth_rate
+    d = parameters.natural_death_rate
+
+    # Totals of subpopulations and whole population
+    n_p = s_p + e_p + i_p + r_p
+
+    # Christian compartments:
+    ds_c = 0
+    de_c = 0
+    di_c = 0
+    dr_c = 0
+    dd_c = 0
+    da_c = 0
+
+    # Pagan compartments:
+    ds_p = b * n_p - beta * s_p * i_p / n_p - d * s_p
+    de_p = beta * s_p * i_p / n_p - sigma * e_p - d * e_p
     di_p = sigma * e_p - gamma * i_p - d * i_p
     dr_p = (1 - fatality_rate) * gamma * i_p - d * r_p
     dd_p = fatality_rate * gamma * i_p
